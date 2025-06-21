a6909a97d55709882d9164cf6a32e644eed570cb
mgarvelaiz <mg.arvelaiz@alumnos.upm.es>
2025-06-21 18:44:07 +0200

    Base sensor interface added. Test integration with i2C and will be done

---
+++ Core/Inc/sensors_interface.h
@@ -0,0 +1,32 @@
+/*
+ * sensors_interface.h
+ *
+ *  Created on: Jun 21, 2025
+ *      Author: magaa
+ */
+
+#ifndef INC_SENSORS_INTERFACE_H_
+#define INC_SENSORS_INTERFACE_H_
+
+
+// Initializes all sensor modules (I2C setup, sensor init, etc.)
+void sensor_interface_init(void);
+
+// Releases sensor resources
+void sensor_interface_deinit(void);
+
+// Called periodically to trigger sensor readings (e.g., from a timer)
+void sensor_interface_schedule_readings(void);
+
+/**
+ * @brief  Handles incoming UART command requests directed to the sensor layer.
+ *         This is typically called from the data handler or UART handler layer.
+ *
+ * @param  cmd_id   The command identifier (see BLE/UART protocol definitions)
+ * @param  payload  Optional payload received with the command
+ * @param  len      Length of the payload
+ */
+void sensor_interface_handle_cmd(uint8_t cmd_id, uint8_t* payload, uint8_t len, uint8_t* payload_out, uint8_t* len_out);
+
+
+#endif /* INC_SENSORS_INTERFACE_H_ */
---
+++ Core/Src/sensors_interface.c
@@ -0,0 +1,70 @@
+/*
+ * sensors_interface.c
+ *
+ *  Created on: Jun 21, 2025
+ *      Author: magaa
+ */
+
+
+#include "sensor_interface.h"
+#include "temp_sensor.h"
+#include "i2c_handler.h"
+#include "uart_handler.h"
+#include <string.h>
+
+// Internal buffer for I2C tx/rx
+static uint8_t i2c_tx_buf[4];
+static uint8_t i2c_rx_buf[8];
+
+void sensor_interface_init(void) {
+    temp_sensor_init(TEMP_SENSOR_ADDRESS);
+    // TODO: Add other sensors
+}
+
+void sensor_interface_deinit(void) {
+    temp_sensor_deinit();
+    // TODO: Add other sensor deinit
+}
+
+void sensor_interface_schedule_readings(void) {
+    Temp_Sensor_t* temp = temp_sensor_get();
+    if (!temp) return;
+
+    // Prepare I2C payload (depends on sensor)
+    temp->prepare_i2c_request(i2c_tx_buf);
+
+    if (i2c_handler_write(temp->i2c_address, i2c_tx_buf, 1)) {
+        // Delay or wait for sensor to be ready (optional)
+        // Read back result
+        if (i2c_handler_read(temp->i2c_address, i2c_rx_buf, 4)) {
+            temp->decode_i2c_response(i2c_rx_buf, 4);
+        }
+    }
+}
+
+void sensor_interface_handle_cmd(uint8_t cmd_id, uint8_t* payload, uint8_t len, uint8_t* payload_out, uint8_t* len_out) {
+
+
+    switch (cmd_id) {
+        case CMD_REQ_TEMP_DATA:
+		if (payload[0] == 0x02) {
+			Temp_Sensor_t* env_temp = temp_sensor_get();
+			if (env_temp && env_temp->get_last_n_data_serial_format) {
+				payload_out[0]=0x02;
+				(*len_out)=env_temp->get_last_n_data_serial_format(1,payload_out+1);
+				(*len_out)= (*len_out)+1;
+			}
+
+        }
+
+            break;
+
+        case CMD_REQ_ALL_DATA:
+            // TODO: Add support for composing full sensor packet with all sensor readings
+            break;
+
+        default:
+            // Unknown command
+            break;
+    }
+}
