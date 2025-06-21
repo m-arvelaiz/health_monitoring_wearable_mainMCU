7e2d9e728f4da89d7b70faedd1f591d1e4743dd1
mgarvelaiz <mg.arvelaiz@alumnos.upm.es>
2025-06-21 20:00:11 +0200

    read and write register functions completed

--- Core/Inc/i2c_handler.h
+++ Core/Inc/i2c_handler.h
@@ -24,6 +24,13 @@ typedef enum {
 
 typedef struct I2C_Handler {
     I2C_HandleTypeDef* hi2c;
+    uint8_t last_address;
+    uint8_t Write_buffer[I2C_MAX_BUFFER_SIZE];
+    uint8_t write_buffer_lenght;
+    uint8_t Response_buffer[I2C_MAX_BUFFER_SIZE];
+    uint8_t response_buffer_lenght;
+    bool (*write_reg)(uint8_t device_addr, uint8_t* data, uint8_t len);
+    bool (*read_reg)(uint8_t device_addr, uint8_t* data_out, uint8_t len);
 } I2C_Handler_t;
 
 // Initialization
@@ -31,8 +38,6 @@ void i2c_handler_init(I2C_HandleTypeDef* hi2c);
 I2C_Handler_t* i2c_handler_get(void);
 void i2c_handler_deinit();
 
-// Basic operations
-bool i2c_handler_write(uint8_t device_addr, uint8_t* data, uint8_t len);
-bool i2c_handler_read(uint8_t device_addr, uint8_t* data_out, uint8_t len);
+
 
 #endif /* INC_I2C_HANDLER_H_ */
--- Core/Src/i2c_handler.c
+++ Core/Src/i2c_handler.c
@@ -11,23 +11,45 @@
 
 static I2C_Handler_t* i2c_handler = NULL;
 
+// Basic operations
+bool i2c_handler_write_reg(uint8_t device_addr, uint8_t* data, uint8_t len);
+bool i2c_handler_read_reg(uint8_t device_addr, uint8_t* data_out, uint8_t len);
 
-bool i2c_handler_write(uint8_t device_addr, uint8_t* data, uint8_t len) {
-    if (!i2c_handler || !i2c_handler->hi2c || !data || len == 0) return false;
 
-    if (HAL_I2C_Master_Transmit(i2c_handler->hi2c, device_addr << 1, data, len, HAL_MAX_DELAY) != HAL_OK) {
+bool i2c_handler_write_reg(uint8_t device_addr) {
+	i2c_handler->last_address=device_addr;
+    if (!i2c_handler || !i2c_handler->hi2c || !i2c_handler->Write_buffer || i2c_handler->write_buffer_lenght == 0) return false;
+
+    if (HAL_I2C_Master_Transmit(i2c_handler->hi2c, device_addr << 1, i2c_handler->Write_buffer, i2c_handler->write_buffer_lenght, HAL_MAX_DELAY) != HAL_OK) {
         return false;
     }
     return true;
 }
 
-bool i2c_handler_read(uint8_t device_addr, uint8_t* data_out, uint8_t len) {
-    if (!i2c_handler || !i2c_handler->hi2c || !data_out || len == 0) return false;
+bool i2c_handler_read_reg(uint8_t device_addr) {
 
-    if (HAL_I2C_Master_Receive(i2c_handler->hi2c, device_addr << 1, data_out, len, HAL_MAX_DELAY) != HAL_OK) {
-        return false;
-    }
-    return true;
+	i2c_handler->last_address = device_addr;
+	if (!i2c_handler || !i2c_handler->hi2c || !i2c_handler->Write_buffer
+			|| i2c_handler->write_buffer_lenght == 0){
+		return false;
+	}
+	if (HAL_I2C_Master_Transmit(i2c_handler->hi2c, device_addr << 1,
+			i2c_handler->Write_buffer, i2c_handler->write_buffer_lenght,
+			HAL_MAX_DELAY) != HAL_OK) {
+		return false;
+	}
+
+	if (!i2c_handler || !i2c_handler->hi2c || !i2c_handler->Response_buffer){
+		return false;}
+
+	//TODO: Check if Interruption needed
+
+	if (HAL_I2C_Master_Receive(i2c_handler->hi2c, device_addr << 1,
+			i2c_handler->Response_buffer, i2c_handler->response_buffer_lenght,
+			HAL_MAX_DELAY) != HAL_OK) {
+		return false;
+	}
+	return true;
 }
 
 
@@ -36,6 +58,10 @@ void i2c_handler_init(I2C_HandleTypeDef* hi2c) {
         i2c_handler = (I2C_Handler_t*) malloc(sizeof(I2C_Handler_t));
     }
     i2c_handler->hi2c = hi2c;
+    memset(i2c_handler->Write_buffer, 0, I2C_MAX_BUFFER_SIZE);
+    memset(i2c_handler->Response_buffer, 0, I2C_MAX_BUFFER_SIZE);
+    i2c_handler->read_reg=i2c_handler_read_reg;
+    i2c_handler->write_reg=i2c_handler_write_reg;
 }
 
 I2C_Handler_t* i2c_handler_get(void) {
